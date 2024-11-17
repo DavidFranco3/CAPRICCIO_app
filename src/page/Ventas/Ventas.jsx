b5f17b3d44b702180d67bcc4db88cf405de0b278
José David Ayala Franco <73260715+DavidFranco3@users.noreply.github.com>
2024-11-16 21:12:32 -0600

    Update Ventas.jsx

--- src/page/Ventas/Ventas.jsx
+++ src/page/Ventas/Ventas.jsx
@@ -1,334 +1,156 @@
-import { useState, useEffect, Suspense } from 'react';
-import { listarPaginacionVentasActivas, totalVentasActivas, listarPaginacionVentasCanceladas, totalVentasCanceladas, listarPaginacionVentasTicketActivas, totalVentasTicketActivas, listarPaginacionVentasTicketCanceladas, totalVentasTicketCanceladas, obtenerMesaOcupadas } from "../../api/ventas";
-import { withRouter } from "../../utils/withRouter";
-import "../../scss/styles.scss";
-import { Alert, Col, Row, Button, Spinner, Form } from "react-bootstrap";
-import ListVentas from "./components/ListVentas";
-import { getTokenApi, isExpiredToken, logoutApi, obtenidusuarioLogueado } from "../../api/auth";
-import { obtenerUsuario } from "../../api/usuarios";
-import { LogsInformativosLogout } from '../Logs/components/LogsSistema/LogsSistema';
+import { useState } from 'react';
+import "../../../../scss/styles.scss";
+import { cancelarVenta } from "../../../../api/ventas";
 import { toast } from "react-toastify";
-import Lottie from "react-lottie-player";
-import AnimacionLoading from "../../assets/json/loading.json";
-import { Switch } from '@headlessui/react';
+import { Button, Col, Row, Form, Spinner, Alert } from "react-bootstrap";
+import queryString from "query-string";
+import { faX, faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import 'dayjs/locale/es';
+import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import { LogsInformativos } from '../../../Logs/components/LogsSistema/LogsSistema';
 
-function Ventas(props) {
-    const { setRefreshCheckLogin, location, navigate } = props;
+function CancelarVenta(props) {
+    const { datosVentas, navigate, location, setShowModal } = props;
 
-    const [numeroTiquet, setNumeroTiquet] = useState("");
-    const [estadoSwitch, setEstadoSwitch] = useState(true);
-    const [estadoUsuario, setEstadoUsuario] = useState("");
-    const [idUsuario, setIdUsuario] = useState("");
-    const [datosUsuario, setDatosUsuario] = useState("");
+    console.log(location);
 
-    
-  
-    
+    const { _id, numeroTiquet, productos, total, estado, createdAt } = datosVentas
 
-    const obtenerDatosUsuario = () => {
+    console.log(navigate)
+
+    dayjs.locale('es');
+    dayjs.extend(localizedFormat);
+
+    // Para cancelar el registro
+    const cancelarRegistro = () => {
+        setShowModal(false)
+    }
+
+    const [loading, setLoading] = useState(false);
+
+    const onSubmit = e => {
+        e.preventDefault()
+        setLoading(true);
         try {
-            obtenerUsuario(obtenidusuarioLogueado(getTokenApi())).then(response => {
+            const dataTemp = {
+                estado: estado === "true" ? "false" : "true"
+            }
+            cancelarVenta(_id, dataTemp).then(response => {
                 const { data } = response;
-                setDatosUsuario(data);
-                setIdUsuario(data._id);
-                setEstadoUsuario(data.admin);
-            }).catch((e) => {
-                if (e.message === 'Network Error') {
-                    toast.error("Conexión al servidor no disponible");
-                }
-            });
+                navigate({
+                    search: queryString.stringify(""),
+                });
+                LogsInformativos("Estado de la venta " + numeroTiquet + " actualizado", datosVentas);
+                toast.success(data.mensaje);
+                cancelarRegistro();
+            }).catch(e => {
+                console.log(e)
+            })
         } catch (e) {
-            console.log(e);
+            console.log(e)
         }
     }
-
-    useEffect(() => {
-        obtenerDatosUsuario();
-    }, []);
-
-    const cierreSesion = () => {
-        if (getTokenApi()) {
-            if (isExpiredToken(getTokenApi())) {
-                LogsInformativosLogout("Sesión finalizada", datosUsuario, setRefreshCheckLogin);
-                logoutApi();
-                setRefreshCheckLogin(true);
-                toast.warning('Sesión expirada');
-                toast.success('Sesión cerrada por seguridad');
-            }
-        }
-    }
-
-    useEffect(() => {
-        cierreSesion();
-    }, []);
-
-    const [listVentas, setListVentas] = useState(null);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
-    const [page, setPage] = useState(1);
-    const [noTotalVentas, setNoTotalVentas] = useState(1);
-
-    const cargarDatos = () => {
-        try {
-            if (estadoSwitch) {
-                if (!numeroTiquet) {
-                    totalVentasActivas().then(response => {
-                        const { data } = response;
-                        setNoTotalVentas(data);
-                    }).catch(e => {
-                        console.log(e);
-                    });
-
-                    if (page === 0) {
-                        setPage(1);
-
-                        listarPaginacionVentasActivas(page, rowsPerPage).then(response => {
-                            const { data } = response;
-                            if (!listVentas && data) {
-                                setListVentas(formatModelVentas(data));
-                            } else {
-                                const datosVentas = formatModelVentas(data);
-                                setListVentas(datosVentas);
-                            }
-                        }).catch(e => {
-                            console.log(e);
-                        });
-                    } else {
-                        listarPaginacionVentasActivas(page, rowsPerPage).then(response => {
-                            const { data } = response;
-                            if (!listVentas && data) {
-                                setListVentas(formatModelVentas(data));
-                            } else {
-                                const datosVentas = formatModelVentas(data);
-                                setListVentas(datosVentas);
-                            }
-                        }).catch(e => {
-                            console.log(e);
-                        });
-                    }
-                } else {
-                    totalVentasTicketActivas(numeroTiquet.toString()).then(response => {
-                        const { data } = response;
-                        setNoTotalVentas(data);
-                    }).catch(e => {
-                        console.log(e);
-                    });
-
-                    if (page === 0) {
-                        setPage(1);
-
-                        listarPaginacionVentasTicketActivas(page, rowsPerPage, numeroTiquet.toString()).then(response => {
-                            const { data } = response;
-                            if (!listVentas && data) {
-                                setListVentas(formatModelVentas(data));
-                            } else {
-                                const datosVentas = formatModelVentas(data);
-                                setListVentas(datosVentas);
-                            }
-                        }).catch(e => {
-                            console.log(e);
-                        });
-                    } else {
-                        listarPaginacionVentasTicketActivas(page, rowsPerPage, numeroTiquet.toString()).then(response => {
-                            const { data } = response;
-                            if (!listVentas && data) {
-                                setListVentas(formatModelVentas(data));
-                            } else {
-                                const datosVentas = formatModelVentas(data);
-                                setListVentas(datosVentas);
-                            }
-                        }).catch(e => {
-                            console.log(e);
-                        });
-                    }
-                }
-            } else {
-                if (!numeroTiquet) {
-                    totalVentasCanceladas().then(response => {
-                        const { data } = response;
-                        setNoTotalVentas(data);
-                    }).catch(e => {
-                        console.log(e);
-                    });
-
-                    if (page === 0) {
-                        setPage(1);
-
-                        listarPaginacionVentasCanceladas(page, rowsPerPage).then(response => {
-                            const { data } = response;
-                            if (!listVentas && data) {
-                                setListVentas(formatModelVentas(data));
-                            } else {
-                                const datosVentas = formatModelVentas(data);
-                                setListVentas(datosVentas);
-                            }
-                        }).catch(e => {
-                            console.log(e);
-                        });
-                    } else {
-                        listarPaginacionVentasCanceladas(page, rowsPerPage).then(response => {
-                            const { data } = response;
-                            if (!listVentas && data) {
-                                setListVentas(formatModelVentas(data));
-                            } else {
-                                const datosVentas = formatModelVentas(data);
-                                setListVentas(datosVentas);
-                            }
-                        }).catch(e => {
-                            console.log(e);
-                        });
-                    }
-                } else {
-                    totalVentasTicketCanceladas(numeroTiquet.toString()).then(response => {
-                        const { data } = response;
-                        setNoTotalVentas(data);
-                    }).catch(e => {
-                        console.log(e);
-                    });
-
-                    if (page === 0) {
-                        setPage(1);
-
-                        listarPaginacionVentasTicketCanceladas(page, rowsPerPage, numeroTiquet.toString()).then(response => {
-                            const { data } = response;
-                            if (!listVentas && data) {
-                                setListVentas(formatModelVentas(data));
-                            } else {
-                                const datosVentas = formatModelVentas(data);
-                                setListVentas(datosVentas);
-                            }
-                        }).catch(e => {
-                            console.log(e);
-                        });
-                    } else {
-                        listarPaginacionVentasTicketCanceladas(page, rowsPerPage, numeroTiquet.toString()).then(response => {
-                            const { data } = response;
-                            if (!listVentas && data) {
-                                setListVentas(formatModelVentas(data));
-                            } else {
-                                const datosVentas = formatModelVentas(data);
-                                setListVentas(datosVentas);
-                            }
-                        }).catch(e => {
-                            console.log(e);
-                        });
-                    }
-                }
-            }
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
-    useEffect(() => {
-        cargarDatos();
-    }, [location, estadoSwitch, page, numeroTiquet, rowsPerPage]);
 
     return (
         <>
-            <Row>
-                <Col xs={12} md={8}>
-                    <h3 className="tituloSwitch">Estado de las ventas</h3>
-                </Col>
-                <Col xs={6} md={4}>
-                    <Switch
-                        title={estadoSwitch === true ? "Ver ventas canceladas" : "Ver ventas activas"}
-                        checked={estadoSwitch}
-                        onChange={setEstadoSwitch}
-                        className={`${estadoSwitch ? 'bg-teal-900' : 'bg-red-600'}
-                            relative inline-flex flex-shrink-0 h-[38px] w-[74px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-                    >
-                        <span className="sr-only">Use setting</span>
-                        <span
-                            aria-hidden="true"
-                            className={`${estadoSwitch ? 'translate-x-9' : 'translate-x-0'}
-                                pointer-events-none inline-block h-[34px] w-[34px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
-                        />
-                    </Switch>
-                </Col>
-            </Row>
+            <div className="datosDelProducto">
 
-            <Row>
-                <Col></Col>
-                <Col>
-                    <Form.Control
-                        id="search"
-                        type="text"
-                        placeholder="Busqueda por numero de ticket"
-                        aria-label="Search Input"
-                        value={numeroTiquet}
-                        onChange={e => setNumeroTiquet(e.target.value)}
-                    />
-                </Col>
-                <Col></Col>
-            </Row>
-
-            {
-                listVentas ?
+                {estado === "true" ?
                     (
                         <>
-                            <Suspense fallback={<Spinner />}>
-                                <ListVentas
-                                    listVentas={listVentas}
-                                    location={location}
-                                    navigate={navigate}
-                                    setRefreshCheckLogin={setRefreshCheckLogin}
-                                    setRowsPerPage={setRowsPerPage}
-                                    rowsPerPage={rowsPerPage}
-                                    page={page}
-                                    setPage={setPage}
-                                    noTotalVentas={noTotalVentas}
-                                    estadoUsuario={estadoUsuario}
-                                />
-                            </Suspense>
+                            <Alert variant="danger">
+                                <Alert.Heading>Atención! Acción destructiva!</Alert.Heading>
+                                <p className="mensaje">
+                                    Esta acción cancelara la venta.
+                                </p>
+                            </Alert>
                         </>
                     )
                     :
                     (
                         <>
-                            <Lottie loop={true} play={true} animationData={AnimacionLoading} />
+                            <Alert variant="success">
+                                <Alert.Heading>Atención! Acción constructiva!</Alert.Heading>
+                                <p className="mensaje">
+                                    Esta acción recuperara la venta.
+                                </p>
+                            </Alert>
                         </>
                     )
-            }
+                }
+                <Form onSubmit={onSubmit}>
+
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridNoTiquet">
+                            Número del ticket
+                            <Form.Control
+                                type="text"
+                                value={numeroTiquet}
+                                disabled
+                            />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridNoProductos">
+                            Productos vendidos
+                            <Form.Control
+                                type="text"
+                                value={productos.length}
+                                disabled
+                            />
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridNoTiquet">
+                            Total
+                            <Form.Control
+                                type="text"
+                                value={total}
+                                disabled
+                            />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridNoProductos">
+                            Día de la venta
+                            <Form.Control
+                                type="text"
+                                value={dayjs(createdAt).format('L hh:mm A')}
+                                disabled
+                            />
+                        </Form.Group>
+                    </Row>
+
+                    <Form.Group as={Row} className="botonSubirProducto">
+                        <Col>
+                            <Button
+                                title={"Eliminar venta"}
+                                type="submit"
+                                variant="success"
+                                className="registrar"
+                            >
+                                <FontAwesomeIcon icon={faTrashCan} /> {!loading ? ("Eliminar") : <Spinner animation="border" />}
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Button
+                                title="Cerrar ventana"
+                                variant="danger"
+                                className="cancelar"
+                                onClick={() => {
+                                    cancelarRegistro()
+                                }}
+                            >
+                                <FontAwesomeIcon icon={faX} /> Cancelar
+                            </Button>
+                        </Col>
+                    </Form.Group>
+
+                </Form>
+            </div>
         </>
     );
 }
 
-function formatModelVentas(ventas) {
-    const tempVentas = [];
-    ventas.forEach((venta) => {
-        tempVentas.push({
-            id: venta._id,
-            numeroTiquet: venta.numeroTiquet,
-            cliente: venta.cliente,
-            mesa: venta.mesa,
-            tipo: venta.tipo ? venta.tipo : "No disponible",
-            usuario: venta.usuario,
-            productosVendidos: venta.productos.length,
-            articulosVendidos: venta.productos,
-            detalles: venta.detalles,
-            tipoPago: venta.tipoPago,
-            efectivo: venta.efectivo,
-            cambio: venta.cambio,
-            total: parseFloat(venta.total),
-            subtotal: parseFloat(venta.subtotal),
-            iva: parseFloat(venta.iva),
-            comision: parseFloat(venta.comision),
-            pagado: venta.pagado,
-            hacerPedido: venta.hacerPedido,
-            tipoPedido: venta.tipoPedido,
-            estado: venta.estado,
-            atendido: !venta.atendido ? "false" : venta.atendido,
-            semana: venta.semana,
-            año: !venta.año ? "2023" : venta.año,
-            semana: !venta.semana ? "0" : venta.semana,
-            fechaCreacion: venta.createdAt,
-            fechaActualizacion: venta.updatedAt
-        });
-    });
-    return tempVentas;
-}
-
-
-
-
-export default withRouter(Ventas);
+export default CancelarVenta;
